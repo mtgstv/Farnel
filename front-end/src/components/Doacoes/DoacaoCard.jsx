1f1125934438e18841b9243704fe0b41cf504e69
Emilyano Vasconcelos <emilyano.vasconcelos123@gmail.com>
2026-09-20 15:37:18 -0300

    renderiza doações dinamicamente

--- front-end/src/components/Doacoes/DoacaoCard.jsx
+++ front-end/src/components/Doacoes/DoacaoCard.jsx
@@ -1,8 +1,22 @@
-function DoacaoCard (){
+function DoacaoCard ({categoria, titulo, quantidade, validade, local, status}){
     return(
         <div>
-            Card da Doação
+
+            <p>{categoria}</p>
+        
+            <h2>{titulo}</h2>
+
+            <p>Quantidade: {quantidade}</p>
+
+            <p>Validade: {validade}</p>
+
+            <p>Local: {local}</p>
+
+            <p>Status: {status}</p>
+
+            <button>Ver detalhes</button>
         </div>
     )
 }
 
+export default DoacaoCard;
--- front-end/src/pages/Doacoes/Doacoes.jsx
+++ front-end/src/pages/Doacoes/Doacoes.jsx
@@ -1,8 +1,27 @@
+import DoacaoCard from "../../components/Doacoes/DoacaoCard";
+import { doacoes } from "../../data/content";
+
 function Doacoes(){
 
     return(
-        
-        <h1>Doações Disponíveis</h1>
+
+        <div>
+            
+            {doacoes.map((doacao) =>(
+                <DoacaoCard
+                    key={doacao.id}
+                    categoria={doacao.categoria}
+                    titulo={doacao.titulo}
+                    quantidade={doacao.quantidade}
+                    validade={doacao.validade}
+                    local={doacao.local}
+                    status={doacao.status}
+                />
+            ))}
+            
+
+        </div>
+
     );
 }
 
